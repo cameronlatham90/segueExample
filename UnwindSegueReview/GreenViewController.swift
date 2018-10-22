e7ac7fe67f84302774414cc8df87d3985bf1751b
Cameron Latham <cameronlatham@boe_17881.barren.ketsds.net>
2018-10-22 10:44:00 -0500

    test

---
+++ UnwindSegueReview/GreenViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GreenViewController.swift
+//  UnwindSegueReview
+//
+//  Created by Cameron Latham on 10/22/18.
+//  Copyright © 2018 Cameron Latham. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBackToBlueTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func goToYellowTapped(_ sender: Any) {
+        performSegue(withIdentifier: "unwindToYellow", sender: self)
+    }
+    
+}
---
+++ UnwindSegueReview/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  UnwindSegueReview
+//
+//  Created by Cameron Latham on 10/22/18.
+//  Copyright © 2018 Cameron Latham. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func unwindToYellowVC(segue: UIStoryboardSegue) { }
+}
